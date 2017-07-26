3c102596a06ecf675191810e02259984e5c2ea0f
BBK-PiJ-2015-87 <ivanovs223@gmail.com>
2017-07-26 22:59:25 +0100

    Simple web server to setup infrastructure.

---
+++ src/main/scala/server/WebServer.scala
@@ -0,0 +1,48 @@
+package server
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+import akka.stream.ActorMaterializer
+import com.typesafe.config.ConfigFactory
+import model.BidRequest
+
+
+
+object WebServer {
+  def main(args: Array[String]) {
+
+    implicit val system = ActorSystem("bidder")
+    implicit val materializer = ActorMaterializer()
+    implicit val executionContext = system.dispatcher
+
+    val config = ConfigFactory.load
+    val host = config.getString("bidder.host")
+    val port = config.getInt("bidder.port")
+
+    val route: Route =
+      get {
+        pathSingleSlash {
+          complete("test")
+        } ~
+        pathPrefix("app_status") {
+          complete("healthy")
+        }
+      } ~
+      post {
+        path("bid") {
+          entity(as[BidRequest]) { bid =>
+              complete(s"BidResponse for received $bid")
+            }
+        }
+      }
+
+    val bindingFuture = Http().bindAndHandle(route, host, port)
+
+    bindingFuture.failed.foreach { ex =>
+      println(ex)
+    }
+  }
+}
+
