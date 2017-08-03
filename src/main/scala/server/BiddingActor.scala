10926e259c795b68c00773c094b3277dd8d94cb5
Vladimirs Ivanovs <vivanovs@mdsol.com>
2017-08-03 14:31:43 +0100

    Make async calls test

--- src/main/scala/server/BiddingActor.scala
+++ src/main/scala/server/BiddingActor.scala
@@ -1,37 +1,44 @@
 package server
 
-import akka.actor.Actor
-import akka.actor.Status.Success
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.pattern.{ask, pipe}
 import akka.util.Timeout
 import model.{BidRequest, BidResponse}
 
-import scala.concurrent.{ExecutionContext, Future, Promise}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 import scala.concurrent.duration._
 
+class Bidder {
+  implicit val system = ActorSystem("bidder_two")
+  implicit val timeout = Timeout(15 seconds)
+  val biddingActor = system.actorOf(Props[BiddingActor], "bidding_actor")
 
-class BiddingActor extends Actor{
+  def request(bid: BidRequest): Future[Option[BidResponse]] = (biddingActor ? bid).mapTo[Option[BidResponse]]
+}
 
-
-  implicit val ec = ExecutionContext.global
-  implicit val timeout = Timeout(45 seconds)
-
-  def response(bid: BidRequest): Future[BidResponse] = {
-    val promise = Promise[Either[BidResponse, Exception]]
+object BiddingActor {
+  def asyncRequest(req: BidRequest): Future[Option[BidResponse]] = {
     Future {
-      bid.ads match {
+      req.ads match {
         case Some(ads) => {
           val bids = ads map (ad => (ad, 5))
           println("Got a request")
           Thread.sleep(5000)
           println(s"Reply is $bids")
-          BidResponse(bid.id * 2, Some(bids))
+          Some(BidResponse(req.id * 2, Some(bids)))
         }
-        case None => BidResponse(bid.id * 2, None)
+        case None => None
       }
     }
   }
+}
+
+class BiddingActor extends Actor {
+
+  import BiddingActor._
 
   override def receive: Receive = {
-    case bid: BidRequest => sender ! response(bid)
+    case req: BidRequest => asyncRequest(req).pipeTo(sender)
   }
 }
--- src/main/scala/server/WebServer.scala
+++ src/main/scala/server/WebServer.scala
@@ -2,39 +2,31 @@ package server
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
+import akka.stream.ActorMaterializer
+import akka.util.Timeout
 import com.typesafe.config.ConfigFactory
 import model.{BidRequest, BidResponse}
 import utils.JacksonHelper._
-import akka.http.scaladsl.server.Directives._
-import akka.stream.ActorMaterializer
-import akka.pattern.ask
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
-import akka.actor._
-import akka.http.scaladsl.model.StatusCodes
-import akka.http.scaladsl.model.StatusCodes.ServerError
-import akka.util.Timeout
-
 import scala.util.{Failure, Success}
-import akka.pattern.ask
-
-import scala.concurrent.{Await, Future}
 
 object WebServer {
   def main(args: Array[String]) {
-
     implicit val system = ActorSystem("bidder")
     implicit val materializer = ActorMaterializer()
-    implicit val executionContext = system.dispatcher
-    implicit val um = unmarshaller[BidRequest]
     implicit val timeout = Timeout(45 seconds)
-
     val config = ConfigFactory.load
     val host = config.getString("bidder.host")
     val port = config.getInt("bidder.port")
 
-    val biddingActor = system.actorOf(Props[BiddingActor], name = "biddingActor")
+    implicit val um = unmarshaller[BidRequest]
+
+    val bidder = new Bidder()
 
     val route: Route =
       get {
@@ -48,10 +40,12 @@ object WebServer {
         post {
           path("bid") {
             entity(as[BidRequest]) { bid =>
-              val future = biddingActor ? bid
-              val res = future.flatMap(b => Future{(toJson(b))})
-              onSuccess(res) {
-                resp => complete(StatusCodes.OK, toJson(resp))
+              entity(as[BidRequest]) { bid =>
+                onComplete(bidder.request(bid)) {
+                  case Success(Some(resp: BidResponse)) => complete(StatusCodes.OK, toJson(resp))
+                  case Success(None) => complete(StatusCodes.NotFound)
+                  case Failure(ex) => complete(StatusCodes.InternalServerError)
+                }
               }
 
             }
@@ -68,4 +62,3 @@ object WebServer {
     }
   }
 }
-
